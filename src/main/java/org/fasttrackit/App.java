81c4f53baf3b23e7e773df49e120da41c8631de6
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-09-09 21:30:03 +0300

    studied polymorphism

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -10,59 +10,59 @@ public class App {
     public static void main(String[] args) {
         // this is a one-line comment
 
-        AutoVehicle autoVehicle = new AutoVehicle();
-
-        Engine engine = new Engine();
-        engine.manufacturer = "Renault";
-        engine.capacity = 1600;
-
-        Car car = new Car(engine);
-        car.setName("Dacia");
-        car.setFuelLevel(60);
-        car.setMileage(8.5);
-        car.doorCount = 5;
-        car.setMaxSpeed(180);
-        car.setFuelType("Gasoline");
-
-        System.out.println("Car object: " + car.toString());
-
-        System.out.println(car.getName());
-        System.out.println(car.getTraveledDistance());
-        System.out.println(car.isRunning());
-
-        System.out.println(car.engine);
-
-        car.accelerate(60, 1);
-
-        car.accelerate(90);
-
-        Engine engine2 = new Engine();
-        engine2.manufacturer = "VW";
-        engine2.capacity = 1600;
-
-        Car car2 = new Car(engine2);
-        car2.setName("Golf");
-
-        car.setName("Skoda");
-
-//        car.name = null;
-
-        // concatenation
-        System.out.println("Car2 name: " + car2.getName());
-        System.out.println("Car1 name: " + car.getName());
-
-//        car = car2;
-
-
-        System.out.println("Modulo examples");
-        System.out.println(4 % 2);
-        System.out.println(4 % 3);
-
-
-        System.out.println("Please enter some text");
-        Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        System.out.println("Input from user: " + text);
+//        AutoVehicle autoVehicle = new AutoVehicle();
+//
+//        Engine engine = new Engine();
+//        engine.manufacturer = "Renault";
+//        engine.capacity = 1600;
+//
+//        Car car = new Car(engine);
+//        car.setName("Dacia");
+//        car.setFuelLevel(60);
+//        car.setMileage(8.5);
+//        car.doorCount = 5;
+//        car.setMaxSpeed(180);
+//        car.setFuelType("Gasoline");
+//
+//        System.out.println("Car object: " + car.toString());
+//
+//        System.out.println(car.getName());
+//        System.out.println(car.getTraveledDistance());
+//        System.out.println(car.isRunning());
+//
+//        System.out.println(car.engine);
+//
+//        car.accelerate(60, 1);
+//
+//        car.accelerate(90);
+//
+//        Engine engine2 = new Engine();
+//        engine2.manufacturer = "VW";
+//        engine2.capacity = 1600;
+//
+//        Car car2 = new Car(engine2);
+//        car2.setName("Golf");
+//
+//        car.setName("Skoda");
+//
+////        car.name = null;
+//
+//        // concatenation
+//        System.out.println("Car2 name: " + car2.getName());
+//        System.out.println("Car1 name: " + car.getName());
+//
+////        car = car2;
+//
+//
+//        System.out.println("Modulo examples");
+//        System.out.println(4 % 2);
+//        System.out.println(4 % 3);
+//
+//
+//        System.out.println("Please enter some text");
+//        Scanner scanner = new Scanner(System.in);
+//        String text = scanner.nextLine();
+//        System.out.println("Input from user: " + text);
 
 
 //        System.out.println("Static variables example:");
@@ -80,13 +80,17 @@ public class App {
 //        System.out.println("Total count in Car class: " + Car.totalCount);
 
 
-        HybridCar hybridCar = new HybridCar();
+        Vehicle hybridCar = new HybridCar();
         hybridCar.setName("cheater");
 
+        // method implementation from object's class not from variable's class
         hybridCar.accelerate(60, 1);
 
         System.out.println("Fuel level: " + hybridCar.getFuelLevel());
         System.out.println("Traveled distance: " + hybridCar.getTraveledDistance());
 
+        // variable type decides what methods can be invoked
+        // type casting
+        ((HybridCar) hybridCar).fly();
     }
 }
--- src/main/java/org/fasttrackit/HybridCar.java
+++ src/main/java/org/fasttrackit/HybridCar.java
@@ -22,4 +22,8 @@ public class HybridCar extends Vehicle {
     protected HybridCar clone() throws CloneNotSupportedException {
         return new HybridCar();
     }
+
+    public void fly() {
+        System.out.println("Yeeey! Flying!");
+    }
 }
