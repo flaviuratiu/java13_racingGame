1eba865762bfc9d4cbdfbb1a8f5e8c942a755274
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-09-16 19:25:54 +0300

    studied if-else statements

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -21,6 +21,16 @@ public class Vehicle {
     }
 
     public double accelerate(double speed, double durationInHours) {
+        if (speed > maxSpeed) {
+            System.out.println("Maximum speed exceeded.");
+            return 0;
+        }
+
+        if (fuelLevel <= 0) {
+            System.out.println("You are out of fuel...");
+            return 0;
+        }
+
         System.out.println(name + " is accelerating with " +
                 speed + " km/h for " + durationInHours + " h.");
 
