b0caf35696574e541f895d00571fe6b6bcf4dddb
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-09-30 19:08:56 +0300

    added logs

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -25,9 +25,11 @@ public class Game {
         int competitorsWithoutFuel = 0;
 
         while (winnerNotKnown && competitorsWithoutFuel < competitors.size()) {
+            System.out.println("New round...\n");
 
             // for-each or enhanced for loop
             for (Vehicle vehicle : competitors) {
+                System.out.println("It's " + vehicle.getName() + "'s turn.");
                 double speed = getAccelerationSpeedFromUser();
                 vehicle.accelerate(speed, 1);
 
